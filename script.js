bc0762f9aeb734ab071beacb4399078481da2866
Natalia <natalia-321s@outlook.com>
2021-05-28 03:36:54 -0300

    Alterações no slider e estilização do banner

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var imagens = document.getElementsByClassName("imagem");
 
 var i = 0;
 
-btn_anterior.onclick = () => {
+function mostrar_anterior(){
   i--;
   for (var img of imagens) {
     if (i == 0) { img.style.left = "0px" }
@@ -16,7 +16,8 @@ btn_anterior.onclick = () => {
   }
 }
 
-btn_proximo.onclick = () => {
+
+ function mostrar_proximo(){
   i++;
   for (var img of imagens) {
     if (i == 0) { img.style.left = "0px" }
@@ -26,3 +27,16 @@ btn_proximo.onclick = () => {
     if (i > 3) { i = 3 }
   }
 }
+ function mostrar_proximo2(){
+  i++;
+  for (var img of imagens) {
+    if (i == 0) { img.style.left = "0px" }
+    if (i == 1) { img.style.left = "-"+img.style.width; console.log('width: ',img) }
+    if (i == 2) { img.style.left = "-450px" }
+    if (i == 3) { img.style.left = "-675px" }
+    if (i > 3) { i = 3 }
+  }
+}
+
+btn_anterior.onclick = mostrar_anterior
+btn_proximo.onclick = mostrar_proximo
